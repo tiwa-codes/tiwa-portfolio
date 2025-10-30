6e99ee036a7adba8784123e3eec93401ffd071b2
Afolabi Tiwalola <62728389+tiwa-codes@users.noreply.github.com>
2025-10-30 15:18:04 +0100

    Initial commit

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,8 @@
+import samfashLogo from './samfash.jpg';
+import cavicLogo from './cavic.png';
+import campusLogo from './campus.jpg';
+import naptinLogo from './naptin.png';
+
 
 export const services = [
     {
@@ -9,41 +14,73 @@ export const services = [
         icon: 'https://csgeeek.github.io/web.png',
     },
     {
-        title: "Photographer",
+        title: "Data  Scientist",
         icon: 'https://csgeeek.github.io/web.png',
     },
 ];
 
-export const name = 'Nithin';
+export const name = 'Tiwalola';
 
 export const experiences = [
     {
-        'company': 'Inncircles Technologies',
-        'role': 'Product Developer',
-        'duration': 'April 2023 - Present',
-        'logo': 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
+        'company': 'Samfash Investment and Printing Service ltd',
+        'role': 'Web Developer',
+        'duration': 'Jan 2024 - May 2024',
+        'logo': samfashLogo,
         'points': [
-            'Involved in dynamic contributions to web and mobile📱 development projects, actively influencing and improving project outcomes.',
-            'Developed multiple shared components to simplify tasks for fellow developers, boosting efficiency and teamwork.',
-            'Enhanced product performance through significant contributions to backend development, optimizing essential components for improved efficiency 📈.',
-            'Implemented data security for the company’s product to provide role-based access control ⚙️.'
+            'Designed and developed the company’s website, establishing its online presence and optimizing for enhanced user experience.',
+            'Led digital marketing efforts, including SEO optimization and Google Analytics setup, increasing website traffic and online visibility.',
+            'Managed social media accounts, creating engaging content to attract and retain customers.📈.',
+            'Provided administrative support by handling front desk operations, updating company records, and performing data entry tasks ⚙️',
+            'Assisted in secretarial duties including scheduling documentation and client communication.'
         ],
-        'url': 'https://inncircles.com/#home',
+        'url': 'https://samfashprintings.com/#home',
     },
     {
-        'company': 'AT & T',
-        'role': 'Technical Intern',
-        'duration': 'Jan 2023 - March 2023',
-        'logo': 'https://www.att.com/ecms/dam/att/consumer/global/logos/att_globe_500x500.jpg',
+        'company': 'Creative Arts and Visual Imagery Center',
+        'role': 'Web Developer & Digital Content Manager',
+        'duration': 'May 2023 - October 2024',
+        'logo': cavicLogo,
         'points': [
-            'Gained experience with Git version control contributing to the development and maintenance of code repositories.',
-            'Worked on the application dashboard, making it more user-friendly and improving its overall functionality📱.'
+            'Spearheaded the development and maintenance of CAVIC’s website, ensuring seamless user experience and accessibility.',
+            'Led a top-five finalist team in an art and technology exhibition, managing the entire incubation process, exhibition setup, and final presentation📱',
+            'Single-handedly coordinated exhibition logistics, including website development, space arrangement, and project pitching.',
+            'Designed and produced engaging visual content, including videos and graphics, for CAVIC’s social media platforms to enhance visibility and audience engagement.',
+            'Created and managed the CAVIC festival website, ensuring it was user-friendly and visually stunning digital content, enhancing the overall aesthetic of the CAVIC festival website.',
+            'Served as the personal assistant to the director, supporting administrative tasks, scheduling, and event coordination.'
         ],
-        'url': 'https://www.att.com/',
+        'url': 'https://www.cavicng.africa/',
     },
-]
-
-
+    {
+        'company': 'Campus Collab',
+        'role': 'Founder & Community Manager',
+        'duration': 'September 2023 - Present',
+        'logo': campusLogo,
+        'points': [
+            'Established and grew a thriving Discord community for Nigerian students in tech, business, arts, and design, fostering knowledge sharing and networking',
+            'Developed and implemented comprehensive brand guidelines, ensuring consistency across all platforms.',
+            'Created and managed social media channels, driving engagement and expanding the community’s reach.📈.',
+            'Recruited and coordinated campus ambassadors across multiple schools, strengthening community presence and student participation.⚙️',
+            'Designed structured career roadmaps and resources to support members in their professional development.'
+        ],
+        'url': 'https://campscolab.com/#home',
+    },
+    {
+        'company': 'National Power Training Institute of Nigeria (NAPTIN)',
+        'role': 'IT Support & Administrative Assistant',
+        'duration': 'May 2022 – November 2022',
+        'logo': naptinLogo,
+        'points': [
+            'Provided IT support to staff, troubleshooting hardware and software issues, and ensuring smooth operation of the institute’s systems.',
+            'Assisted in the development and implementation of the institute’s website, enhancing its functionality and user experience.',
+            'Managed the institute’s social media accounts, creating and scheduling engaging content to attract and retain followers.📱.',
+            'Performed data entry and updated company records, ensuring accuracy and organization of information.',
+            'Supported administrative tasks, including scheduling, documentation, and client communication.',
+            'Monitored and maintained the company’s internet connectivity, diagnosing and resolving network issues.'
+        ],
+        'url': 'https://www.naptin.gov.ng/',
+    },
+];
 
 export const EMAIL_JS_SERVICE_ID = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
 export const EMAIL_JS_TEMPLATE_ID = process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID;
