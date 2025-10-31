3358d5bb634c20ca2ca90f0ace2bae6a84a1418f
Afolabi Tiwalola <62728389+tiwa-codes@users.noreply.github.com>
2025-10-31 14:42:11 +0100

    added images and certificates

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -1,6 +1,15 @@
 import React, { useState } from 'react';
 import cert1 from '../assets/cert1.png';
 import cert2 from '../assets/cert2.png';
+import cert3 from '../assets/cert3.png';
+import cert4 from '../assets/cert4.jpg';
+import cert5 from '../assets/cert5.jpg';
+import cert6 from '../assets/cert6.jpg';
+import cert7 from '../assets/cert7.jpg';
+import cert8 from '../assets/cert8.jpg';
+import cert9 from '../assets/cert9.jpg';
+import cert10 from '../assets/cert10.png';
+import cert11 from '../assets/cert11.png';
 
 const CertificateCard = ({ cert, onOpen }) => (
     <div className="bg-gray-900 border border-neutral-100 rounded-lg shadow p-3">
@@ -19,17 +28,80 @@ const Certificates = () => {
 
     const certificates = [
         {
-            title: 'Frontend Development',
-            issuer: 'FreeCodeCamp',
-            date: 'Mar 2024',
+            title: 'AI Career Essentials',
+            issuer: 'ALX',
+            date: 'Jul 2024',
             image: cert1,
             link: ''
         },
         {
-            title: 'React - The Complete Guide',
-            issuer: 'Udemy',
-            date: 'Jul 2023',
+            title: 'Virtual Assistant',
+            issuer: 'ALX',
+            date: 'Nov 2024',
             image: cert2,
+            link: ''
+        },
+        {
+            title: 'Data Science',
+            issuer: '3MTT',
+            date: 'Dec 2024',
+            image: cert3,
+            link: ''
+        },
+        {
+            title: 'English for IT 1',
+            issuer: 'Cisco Networking Academy',
+            date: 'June 2024',
+            image: cert4,
+            link: ''
+        },
+        {
+            title: 'English for IT 2',
+            issuer: 'Cisco Networking Academy',
+            date: 'June 2024',
+            image: cert5,
+            link: ''
+        },
+        {
+            title: 'Career Essentials in Data Analysis',
+            issuer: 'Microsoft & Linkedin',
+            date: 'July 2024',
+            image: cert6,
+            link: ''
+        },
+        {
+            title: 'Career Essentials in Generative AI',
+            issuer: 'Microsoft & Linkedin',
+            date: 'Jul 2024',
+            image: cert7,
+            link: ''
+        },
+        {
+            title: 'Build Your Generative AI Productivity Skills',
+            issuer: 'Microsoft & Linkedin',
+            date: 'Jul 2024',
+            image: cert8,
+            link: ''
+        },
+        {
+            title: 'Professional Soft Skills Learning Pathway',
+            issuer: 'Linkedin',
+            date: 'Jul 2024',
+            image: cert9,
+            link: ''
+        },
+        {
+            title: 'Transforming Nigeria Youth',
+            issuer: 'EDP Pan-Atlantic University',
+            date: 'Jul 2024',
+            image: cert10,
+            link: ''
+        },
+        {
+            title: 'Evaluation and Light Customization of LLM Models',
+            issuer: 'NVIDIA Deep Learning Institute',
+            date: 'Jul 2025',
+            image: cert11,
             link: ''
         }
     ];
