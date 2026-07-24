764f3b587ad74e5c557908b263f481e2ff651ba5
Afolabi Tiwalola <tiwalolagold42@gmail.com>
2026-07-24 13:54:01 +0100

    Deploy legal pages, GitHub calendar fix, and coming-soon state polish

---
+++ _next/static/chunks/app/legal/terms/page-333607126b33284f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[831,160,308,683,241],{4243:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=4243)}),_N_E=n.O()}]);
